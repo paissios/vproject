78fda3d55978f6ecc240d3e8dd24133c82a12a4c
paissios <paissios.metni@gmail.com>
2024-03-16 17:12:41 +0200

    implemented the login feature including form validation, functionality, responsive design and styling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import LayoutWrapper from "../components/LayoutWrapper/LayoutWrapper";
+import LoginForm from "../components/LoginForm/LoginForm";
 
 const LoginPage = () => {
-  return <LayoutWrapper>LoginPage</LayoutWrapper>;
+  return (
+    <LayoutWrapper>
+      <LoginForm />
+    </LayoutWrapper>
+  );
 };
 
 export default LoginPage;
