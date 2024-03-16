ab4c3cd7fb6450dfde480c5e81962557f0b2142d
paissios <paissios.metni@gmail.com>
2024-03-16 14:21:33 +0200

    cleaned up files and created protected and public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import PrivateRoutes from "./routes/PrivateRoutes";
+import PublicRoutes from "./routes/PublicRoutes";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<PrivateRoutes />}>
+        <Route element={<HomePage />} path="/" />
+        <Route element={<NotFoundPage />} path="*" />
+      </Route>
+      <Route element={<PublicRoutes />}>
+        <Route element={<LoginPage />} path="/login" />
+      </Route>
+    </Routes>
   );
 }
 
